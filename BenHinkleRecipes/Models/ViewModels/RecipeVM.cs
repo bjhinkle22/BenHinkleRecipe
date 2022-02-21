eeee910c31b2ebe8589596701025d89e3dac7e3a
Hinkle, Benjamin J <bjhinkle22@gmail.com>
2022-02-20 18:04:44 -0600

    fixed stuff that broke itself

--- BenHinkleRecipes/Models/ViewModels/RecipeVM.cs
+++ BenHinkleRecipes/Models/ViewModels/RecipeVM.cs
@@ -9,6 +9,7 @@ namespace BenHinkleRecipes.Models.ViewModels
     public class RecipeVM
     {
         [HiddenInput]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int RecipeId { get; set; }
 
         [Required]
@@ -19,28 +20,19 @@ namespace BenHinkleRecipes.Models.ViewModels
         public string? Miscellaneous { get; set; }
 
         [DisplayName("Upload Front of Recipe Photo")]
-        [Required]
         public IFormFile? RecipePhotoFrontFile { get; set; }
 
         [DisplayName("Upload Back of Recipe Photo")]
-        [Required]
         public IFormFile? RecipePhotoBackFile { get; set; }
         public string? Description { get; set; }
         public bool? IsFavorite { get; set; }
 
-        [Required]
-        [DisplayName("Name of Recipe Front Photo")]
-        public string? RecipePhotoFrontName { get; set; }
-
-        [Required]
-        [DisplayName("Name of Recipe Back Photo")]
-        public string? RecipePhotoBackName { get; set; }
-
         public string? RecipeFrontDisplay { get; set; }
 
         public string? RecipeBackDisplay { get; set; }
-
-        public byte[] OriginalRecipeFront { get; set; }
-        public byte[] OriginalRecipeBack { get; set; }
+        [HiddenInput]
+        public byte[]? OriginalRecipeFront { get; set; }
+        [HiddenInput]
+        public byte[]? OriginalRecipeBack { get; set; }
     }
 }
